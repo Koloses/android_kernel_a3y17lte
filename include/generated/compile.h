afc7d1fa8d06a83a7250d4b88628b4197570d393
Krzysztof Kolendowicz <koloses@gmail.com>
2019-02-27 08:19:15 +0100

    Merge A320FLXXU2CRE3

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#0 SMP PREEMPT Wed Feb 27 09:12:29 CET 2019"
+#define LINUX_COMPILE_BY "koloses"
+#define LINUX_COMPILE_HOST "NebenWelt"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.18.15-gc5be2893a05"
